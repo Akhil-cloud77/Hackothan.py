9e10edc15c2a51a47b20a10fe5d71d1f3d458039
Akhil-cloud77 <akhilkona66@gmail.com>
2026-07-04 11:47:28 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,3 +1,7 @@
+
+console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("Supabase Key:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
